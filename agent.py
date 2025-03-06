5f78bc657ee42699dde30012bb725c66d7910322
navgup <navgup@stanford.edu>
2025-03-05 21:20:26 -0800

    swag

--- agent.py
+++ agent.py
@@ -8,6 +8,33 @@ from datetime import datetime, timedelta
 MISTRAL_MODEL = "mistral-large-latest"
 SYSTEM_PROMPT = "You are a helpful assistant."
 
+CLASSIFY_MESSAGE_PROMPT = """
+
+    Classify the message as one of the following categories:
+    1. new feedback
+    2. question about menu
+    3. question about feedback
+
+    Return just the name of the category and nothing else. If the message does not fall 
+    into any of the categories, return "None". 
+
+    Example:
+    Message: Dinner today was great!
+    Response: new feedback
+
+    Example: 
+    Message: Whats for lunch tomorrow?
+    Response: question about menu
+
+    Example:
+    Message: What did people think of the food today?
+    Response: question about feedback
+
+
+    """
+
+
+
 
 class KitchentBotAgent:
     def __init__(self):
@@ -59,6 +86,7 @@ class KitchentBotAgent:
         # The simplest form of an agent
         # Send the message's content to Mistral's API and return Mistral's response
 
+<<<<<<< Updated upstream
         # if message has a csv file, add new menu
         if message.attachments:
             for attachment in message.attachments:
@@ -66,10 +94,12 @@ class KitchentBotAgent:
                     # Read the CSV content as text
                     csv_content = await attachment.read()
                     self.add_new_menu(csv_content)
-                    return "Menu added successfully!"
+=======
+
+>>>>>>> Stashed changes
 
         messages = [
-            {"role": "system", "content": SYSTEM_PROMPT},
+            {"role": "system", "content": CLASSIFY_MESSAGE_PROMPT},
             {"role": "user", "content": message.content},
         ]
 
@@ -77,5 +107,6 @@ class KitchentBotAgent:
             model=MISTRAL_MODEL,
             messages=messages,
         )
+
 
         return response.choices[0].message.content
