b8469ae2047262e86e7701ca815bca81982a8844
alex-gugu <123117867+alex-gugu@users.noreply.github.com>
2025-03-05 21:10:06 -0800

    add functions to be called

--- agent.py
+++ agent.py
@@ -3,6 +3,7 @@ from mistralai import Mistral
 import discord
 from feedback import Feedback
 from menus import Menus
+from datetime import datetime
 
 MISTRAL_MODEL = "mistral-large-latest"
 SYSTEM_PROMPT = "You are a helpful assistant."
@@ -14,6 +15,37 @@ class KitchentBotAgent:
         self.feedback = Feedback()
         self.menus = Menus()
         self.client = Mistral(api_key=MISTRAL_API_KEY)
+
+    def add_new_menu(self, message: str):
+        """
+        Add a new menu to the menus class
+        """
+        this_week_start_date = datetime.now().date()
+        # Adjust to previous Sunday if not already Sunday
+        this_week_start_date = this_week_start_date - datetime.timedelta(
+            days=this_week_start_date.weekday()
+        )
+        self.menus.add_menu(this_week_start_date, message)
+
+    def add_new_feedback(self, user: str, message: str):
+        """
+        Add a new feedback to the feedback class
+        """
+        self.feedback.add_feedback(user, message)
+
+    def question_about_menu(self, message: str):
+        """
+        Question about the menu
+        """
+        # TODO: Arnav implement this
+        pass
+
+    def question_about_feedback(self, message: str):
+        """
+        Question about the feedback
+        """
+        # TODO: Arnav implement this
+        pass
 
     async def run(self, message: discord.Message):
         """
